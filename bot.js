a19c7f9db09fb3d2e586656514f65349f1180a1a
RemArgaill <38848930+RemArgaill@users.noreply.github.com>
2019-04-27 12:29:36 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content === 'ping') {
+   let scrag = client.user.displayAvatarURL;
+let embed = new Discord.RichEmbed()
+.setTitle("Пинг!")
+.setAuthor("ScragBot", scrag)
+.setDescription(`Понг! Задержка - ${msg.createdTimestamp - msg.createdTimestamp}мс. Задержка хостинга - ${Math.round(client.ping)}мс.`)
+.setColor("#009482");
+   msg.channel.send(embed);
+ }
+client.on('guildMemberAdd', member => {
+var role1 = msg.guild.roles.find('name', 'ᅠ ᅠ ᅠ 🔻Ранг🔻');
+var role2 = msg.guild.roles.find('name', 'ᅠ ᅠ ᅠ 🔻Игровые роли🔻');
+var role3 = msg.guild.roles.find('name', 'ᅠ ᅠ ᅠ 🔻Прочее🔻');
+var role4 = msg.guild.roles.find('name', '☀️Пользователь☀️');
+member.addRole(role1,role2,role3,role4);
+   });
+ });
+
+client.login('NTcxMjE0MjU2NTQwODc2ODIw.XMMBWg.8TOHM-q8w_UXoK-3QHZp2yAcNbU');
